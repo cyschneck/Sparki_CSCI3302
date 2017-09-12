6d5a96a105c8d00f960c181cd9c182d90c72a3d1
cschneck <cschneck.noreply.github.com>
2017-09-12 17:13:24 -0600

    line following example in function

--- Labs/Lab1/Lab1.ino
+++ Labs/Lab1/Lab1.ino
@@ -16,7 +16,45 @@ void setup() {
 // | 66  82  74 |
 // \____________/
 
+void defaultLineFollowing() {
+  // line following from examples
+  int threshold = 700;
+  
+  int lineLeft   = sparki.lineLeft();   // measure the left IR sensor
+  int lineCenter = sparki.lineCenter(); // measure the center IR sensor
+  int lineRight  = sparki.lineRight();  // measure the right IR sensor
 
+  if ( lineCenter < threshold ) // if line is below left line sensor
+  {  
+    sparki.moveForward(); // move forward
+  }
+  else{
+    if ( lineLeft < threshold ) // if line is below left line sensor
+    {  
+      sparki.moveLeft(); // turn left
+    }
+  
+    if ( lineRight < threshold ) // if line is below right line sensor
+    {  
+      sparki.moveRight(); // turn right
+    }
+  }
+
+  sparki.clearLCD(); // wipe the screen
+  
+  sparki.print("Line Left: "); // show left line sensor on screen
+  sparki.println(lineLeft);
+  
+  sparki.print("Line Center: "); // show center line sensor on screen
+  sparki.println(lineCenter);
+  
+  sparki.print("Line Right: "); // show right line sensor on screen
+  sparki.println(lineRight);
+  
+  sparki.updateLCD(); // display all of the information written to the screen
+
+  delay(100); // wait 0.1 seconds
+}
 void moveLeftGran(int dist) {
   sparki.moveLeft(300);
   sparki.moveRight(200);
@@ -24,12 +62,12 @@ void moveLeftGran(int dist) {
 
 int readRemote() {
   int code = sparki.readIR();
-   if (code != -1){
+   if (code != -1)
+   {
     sparki.print("Code received in function: ");
     sparki.println(code);
     return code;
    }
-  }
 }
 
 void loop() {
