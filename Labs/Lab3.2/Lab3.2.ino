2e31dbf4eead8b9a64018e4a18e67f3c8ad5b9ce
cschneck <cschneck.noreply.github.com>
2017-10-06 11:55:50 -0600

    codeshare update

--- Labs/Lab3.2/Lab3.2.ino
+++ Labs/Lab3.2/Lab3.2.ino
@@ -1,70 +1,216 @@
-/*******************************************  
-  Lab 3.1:
-  Sparki Odometry
 
-  Summary: Track and print X, Y displacement from 
-  the start line (0.0 m, 0.0m, 0.0 degrees) as
-  well as heading
+
+#include "stdio.h"
+#include <Sparki.h> // include sparki library
+//#include "conio.h"
+#define infinity 999
+
+/* Notes from Correll:
+ for 4x4 graph:
+ 12  3 4
+ 
+ x coord = i % 4
+ y coord = i / 4
+ cost from index i to index j:
+*/
+
+
+/* Pseudo:
+ - we need:
+     - grid[16] filled with 0 (free) or 1 (obstacle) --> have an array obstacle, currently filled for the test case.
+    - index to [x, y] coordinates (int) function: i%4 = x, i/4 = y --> function1 = x,y to 0-15, function2 = 0-15 to x,y
+    - goto[16] array contains lowest cost route to go towards source
+      - value = which node to go to next (e.g. 2 = 'go to 2 next')
+        - e.g. for:
+          0   0   0   0
+          1   1   0   1
+          0   0   0   0
+          0   1   0g  1
+        - goto would look like: 
+          1   2   5   2
+          inf inf 10  inf
+          9   10  14  10  
+          8   inf 14  inf
+    - check for adjacents:
+      - up: i - 4 > 0
+      - down: i + 4 < 15
+      - left: i%4 != 0 (equivalent to i%4 > 0, in this case)
+      - right: i%4 != 3 (equivalent to i%4 < 3, in this case)
+    - if any of these are valid, 
+    - update costs to each of these, I think?
+    
+      
+      [0  01 02 03]
+      [04 05 06 07]
+      [08 09 10 11]
+      [12 13 14 15]
+*/
+
+/*
+cost(i, j) {
+    x1 = i % 4;
+    x2 = j % 4;
+    y1 = i / 4;
+    y2 = j / 4;
+    if (grid[x1][y1] || grid[x2][y2]) { return infinity; }
+    i -> x,y
+    else { return abs(x1-x2) + abs(y1-y2) }
+}
+*/
+
+// v = vertex (node2)
+// n = node
+// dist = distance
+// flag = whether that node has been visited already (i.e. frozen)
+
+/* From online Dijkstra to our code:
+  - graph reference name?
+  - update min=99 to use #defined infinity (999)? (DONE)
+  - 
+*/
+
+/* Test graph with obstacles (o):
+  0   1o  2o  3
+  4   5   6   7
+  8o  9   10o 11o
+  12  13  14  15
   
-  Team: Spark-E
-  Cora Schneck
-  Kylee Bennett
-  Max Messenger Bouricius
-  Anthony Hauger
-  Douglas Allen
-********************************************/
+  Shortest path from 0 to 15 should be [0, 4, 5, 9, 13, 14, 15]
+*/
 
-#include <Sparki.h> // include the sparki library
-
-// Size fo the map for Sparki
-#define  GRID_WIDTH  15.25
+//defining grid constants
+#define GRID_WIDTH  15.25
 #define GRID_HEIGHT 11.75
-// Pixels on the Sparki display screen
-#define PIXEL_WIDTH 31.8725
-#define PIXEL_HEIGHT  15.98
+//array and matrix initialization
+int distance[16]; //distance to get from start node to -a- node -> should be filled with 1's and INF's
+int cost[4][4]; //distance to get from start node to end node -> should look something like (if going from 0 to 4) [0, 1, 2, 3][INF, INF, INF, INF]...
+short int grid = [0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0]; //graph of 0's and 1's -> 0==no obstacle, 1==obstacle --> made for test case currently
+short int flags[16];
+//initializing start and end nodes
+int start = 0;
+int goal = 15;
+int node = 15;
 
-unsigned long start_time; // start time recaculuated at the beginning of each loop
-const float pi = 3.1415926535897932384626;
-// 30 cm travelled in 10773 milliseconds = 30/10773 cm/milliseconds
-    // 30/10.773 cm/s = 2.7847 cm/s
-    // 0.3/10.773 m/s= 0.02785 m/s
-double velocity = 0.02785; // m/s
-double ax_length = 0.0851; // m or 8.51 cm
-int servoDir = 1;     // 1 = moving right, -1 = moving left
-int servoCur = 0;   // Current angle of servo
-int servoSpd = 5;   // Speed of servo rotation (in degrees per 100ms)
-int servoRange = 60;  // How much servo can rotate in either diretion (degrees)
-int coordinates[2];
-int obstacle_graph[16]; //if index == 0, obstacle; if index == 1, navigable
-int distance[4][4];
+void dijkstra_algorithm(int node, int v, int cost[4][4], int distance[])
+{
+    int i,u,count,w,flag[16],min;
+    for(i=0;i<=node;i++)
+    {
+      for(j=0;j<=4; j++){
+        flag[i]=0,distance[i]=check_adjacency(v, i);
+      }
+    }
+    count=2;
+    while(count<=node)
+    {
+        min=infinity;
+        for(w=0;w<=node;w++)
+        {
+            if(distance[w]<min && !flag[w])
+                min=distance[w],u=w;
+        }
+        flag[u]=1;
+        count++;
+        for(w=0;w<=node;w++)
+        {
+            if((distance[u]+cost[u][w]<distance[w]) && !flag[w])
+                distance[w]=distance[u]+cost[u][w];
+        }
+    }
+}
 
-float range = 0;      // Ping value (cm?)
+// Here we're checking if the two points are adjacent, if so, we add the cost.
+int check_adjacency(int *point_1, int *point_2){
+    int grid_distance = math.abs(math.abs(xi - xj) + math.abs(yi - yj));
+      if (grid > 1){
+        return infinity;
+      } else {
+        return 1;
+      }
+}
+
+int checkAdjacent(int i, int j) {
+  // Checks if node with index j is adjacent to i
+  // Not sure if this works yet! I basically just vomited my thought process into codeshare. Somebody verify please?
+  // j = up
+  if ((i - 4) == j) {
+    return 1;
+  }
+  // j = down
+  if ((i + 4) == j) {
+    return 1;
+  }
+  // j = left
+  if ((i % 4) == (j % 4) - 1) {
+    return 1;
+  }
+  // j = right
+  if ((i % 4) == (j % 4) + 1) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
   
-// time it takes to rotate 360 = 37.4961 degrees/s
-// time to move 360 left = 9601 ms
-// time to move 360 right = 9599 ms
-// time to move 360 = 9600 ms = 9.6 seconds
-double speed_rotation = 37.4961; // degrees/second
+  int up = (i - 4 >= 0);
+  int down = (i + 4 <= 15);
+  int left = (i % 4 != 0);
+  int right = (i % 4 != 3);
+  if (up || down || left || right) {
+    return 1;
+  } else {
+    return 0;
+}
 
-float posX, posY = 0.0;
-float theta = 0.0; // rotates around in degrees 
-
-int threshold = 500; // white > 500, black < 500
-
-bool lineLeft, lineCenter, lineRight;
-
+int main()
+{
+    int node,v,i,j,cost[10][10],distance[10];
+    sparki.clearLCD();
+    
+    
+//    printf("n Enter the number of nodes:");
+//    scanf(&node);
+    
+    /* 
+    printf("n Enter the cost matrix:n");
+    for(i=0;i<=node;i++)
+        for(j=1;j<=node;j++)
+            {
+                scanf(&cost[i][j]);       // Gutting this sytem in favor of check_adjacency function
+                if(cost[i][j]==0)
+                    cost[i][j]=infinity;
+            }
+    */
+//    printf("n Enter the source matrix:");
+//    scanf(&v);
+  
+    node = start;
+    //v = grid[];
+  
+    dijkstra_algorithm(node,v,cost,distance);
+    sparki.println("n Shortest path:n");
+    
+    for(i=0;i<=node;i++)
+        if(i!=v)
+    printf("%d->%d,cost=%dn",v,i,distance[i]);
+    
+    //getch();
+}
+  
+/*
 //function 1 - "assigns a single number to each index of your 2D map"
 int grid_to_graph(int x, int y){ //takes an x and y coordinate as input and returns a single number representing the graph node number
-  int graph_i = (int) x/GRID_WIDTH; //grid_width and height defined above
-  int graph_j = (int) y/GRID_HEIGHT;
+  int graph_j = (int) x/GRID_WIDTH; //grid_width and height defined above
+  int graph_i = (int) y/GRID_HEIGHT;
   int graph_value = (4*graph_i)+graph_j; //multiply to find 0-15 value
   
   return graph_value;
 }
+
 //function 2 - "one that takes an index and returns the 2D coordinates"
 int * graph_to_xyCoordinates(int graph_index){ //takes in the graph index and returns an array that acts as a tuple.
-  float x_value = (graph_index/4)*GRID_WIDTH; //divide by four to get i value
-  float y_value = (graph_index%4)*GRID_WIDTH; //modulo by four to get j
+  float x_value = (graph_index%4)*GRID_WIDTH; //divide by four to get i value
+  float y_value = (graph_index/4)*GRID_WIDTH; //modulo by four to get j
   coordinates[0] = x_value;
   coordinates[1] = y_value;
 
@@ -78,7 +224,7 @@ int distance_to(int graph_index, int graph_go_to){
   {
     while (j<4)
     {
-      distance[i][j]=99;
+      distance[i][j]=infinity;
       j++;
     }
     i++;
@@ -91,7 +237,7 @@ int distance_to(int graph_index, int graph_go_to){
 
   if (obstacle_graph[graph_go_to] == 1)
   { //once we get object placing, this ensures that if there's an object we don't move there
-    return 99;
+    return infinity;
   }
   else{
     int distance = abs(graph_index - graph_go_to); //if distance is 1 or 4, the spaces are adjacent
@@ -99,15 +245,39 @@ int distance_to(int graph_index, int graph_go_to){
       return 1; //return 1 for your distance as the grid is one space away
     }
     else{
-      return 99; //not adjacent, large amount to ensure we don't accidentally take that route.
+      return infinity; //not adjacent, large amount to ensure we don't accidentally take that route.
     }
   }
 }
+*/
+
+
+
+/* LAB 2: RETURNS THE POSITION OF THE SPARKI ROBOT
+unsigned long start_time; // start time recaculuated at the beginning of each loop
+const float pi = 3.1415926535897932384626;
+// 30 cm travelled in 10773 milliseconds = 30/10773 cm/milliseconds
+    // 30/10.773 cm/s = 2.7847 cm/s
+    // 0.3/10.773 m/s= 0.02785 m/s
+double velocity = 0.02785; // m/s
+double ax_length = 0.0851; // m or 8.51 cm
+
+// time it takes to rotate 360 = 37.4961 degrees/s
+// time to move 360 left = 9601 ms
+// time to move 360 right = 9599 ms
+// time to move 360 = 9600 ms = 9.6 seconds
+double speed_rotation = 37.4961; // degrees/second
+
+float posX, posY = 0.0;
+float theta = 0.0; // rotates around in degrees 
+
+int threshold = 500; // white > 500, black < 500
+
+bool lineLeft, lineCenter, lineRight;
 
 void setup()
 {
   sparki.clearLCD();
-  sparki.servo(0);
 }
 
 void resetAtStart(bool lineRight, bool lineCenter, bool lineLeft)
@@ -121,48 +291,11 @@ void resetAtStart(bool lineRight, bool lineCenter, bool lineLeft)
     }
 }
 
-void displayPositionAndHeading(float posX, float posY, float theta)
-{
-  sparki.clearLCD(); // wipe the screen
-  
-  sparki.print("X: ");
-  sparki.print(posX);
-  sparki.println(" cm"); // print x displacement in meters
-  
-  sparki.print("Y: ");
-  sparki.print(posY);
-  sparki.println(" cm");
-
-  sparki.print("Theta:");
-  sparki.print(theta); 
-  sparki.println(" degrees"); // print y displacement in meters
-  sparki.print("Ping: ");
-  sparki.println(range);
-  
-  sparki.updateLCD(); // display all of the information written to the screen
-}
-
 void loop() {
   start_time = millis();
   while (millis() < start_time + 100){
     // Wait for sparki to move for 100 milliseconds
   }
-  
-  // Sweep with servo while moving
-  if ((servoCur + (servoDir*servoSpd) > servoRange) || (servoCur + (servoDir*servoSpd) < servoRange*(-1))) {   // If servo at either end, then flip direction
-    if (servoCur > 0) {
-      servoCur = servoRange - (servoSpd*2);
-      servoDir = -1;
-    }
-    if (servoCur <= 0) {
-      servoCur = (servoRange * (-1)) + (2*servoSpd);
-      servoDir = 1;
-    }
-  }
-  servoCur += (servoDir*servoSpd);
-  sparki.servo(servoCur);
-  range = sparki.ping();
-
   lineLeft   = sparki.lineLeft() < threshold;   // measure the left IR sensor (is black)
   lineCenter = sparki.lineCenter() < threshold; // measure the center IR sensor (is black)
   lineRight  = sparki.lineRight() < threshold;  // measure the right IR sensor (is black)
@@ -170,12 +303,12 @@ void loop() {
   if (lineCenter) // if the center line is black
   {  
     sparki.moveForward(); // move forward
-    posX = posX + (cos(theta) * (velocity * 10));
-    posY = posY + (sin(theta) * (velocity * 10));
+    posX = posX + (cos(theta) * (velocity * 0.1));
+    posY = posY + (sin(theta) * (velocity * 0.1));
 
     resetAtStart(lineRight, lineCenter, lineLeft); // if center line is black, checks to see if has returned to origin
   }
-  else {
+  else{
     if (lineLeft) // if the left line is black
     {  
       sparki.moveLeft(); // course correct if left line is black (move left until it is white)
@@ -189,37 +322,6 @@ void loop() {
       theta = fmod(theta, 360); // fmod (floating point module) 360 to keep theta between 0-360
     }
   }
-
-  displayPositionAndHeading(posX, posY, theta);
 }
-
-
-
-
-
-
-
-
-
-
-/*
-61cm wide -> 128px wide
-61/4=15.25cm between [0][0] and [0][1]
-128/61=2.09px/cm
-61/4=15.25cm*2.09px/cm=31.8725px
-47cm tall -> 64px tall
-47/4=11.75cm between [0][0] and [1][0]
-47/64=1.36px/cm
-47/4=11.75cm*1.36px/cm=15.98px
-*/
-/*
-60cm wide -> 128px wide
-60/4=15cm [0][0]->[0][1]
-128/60=2.13px/cm
-60/4=15cm*2.13px/cm=31.95px distance between [0][0]->[0][1]
-48cm tall -> 64px tall
-48/4=12cm [0][0]->[1][0]
-64/48=1.33px/cm
-48/4=12cm*1.33px/cm=15.96px distance between [0][0]->[1][0]
 */
 
