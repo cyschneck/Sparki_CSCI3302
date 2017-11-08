c5475d9b349bbfc758ec05bd006ba08fc0df2fcd
Anthony Hauger <anthonyhauger@Anthonys-MacBook-Air.local>
2017-11-07 19:09:02 -0700

    Added rough ideas for drawings to include in code

---
+++ final_project/LCD_Drawrings/draw_face_code.ino
@@ -0,0 +1,48 @@
+
+void draw_face(){
+  sparki.clearLCD();
+  //mask outline
+  sparki.drawLine(51, 7, 75, 7); //top
+  sparki.drawLine(51, 7, 39, 15); //diagonals to side
+  sparki.drawLine(75, 7, 87, 15);
+  sparki.drawLine(39, 15, 39, 35); //sides
+  sparki.drawLine(87, 15, 87, 35);
+  sparki.drawLine(39, 35, 59, 55); //diagonals to chin
+  sparki.drawLine(87, 35, 67, 55);
+  sparki.drawLine(59, 55, 67, 55); //chin
+  //mask featrues
+  //mouth
+  sparki.drawLine(59, 43, 67, 43);
+  sparki.drawLine(59, 43, 55, 47);
+  sparki.drawLine(55, 47, 71, 47);
+  sparki.drawLine(67, 43, 71, 47);
+  //nose
+  sparki.drawLine(61, 31, 65, 31);
+  sparki.drawLine(63, 31, 61, 35);
+  sparki.drawLine(63, 31, 65, 35);
+  sparki.drawLine(59, 35, 61, 35);
+  sparki.drawLine(65, 35, 67, 35);
+  sparki.drawLine(61, 35, 59, 35);
+  sparki.drawLine(65, 31, 67, 35);
+  //eyes
+  sparki.drawLine(47, 19, 55, 19);
+  sparki.drawLine(47, 23, 55, 23);
+  sparki.drawLine(55, 19, 55, 23);
+  sparki.drawLine(47, 19, 47, 23);
+  sparki.drawLine(67, 19, 67, 23);
+  sparki.drawLine(67, 19, 79, 19);
+  sparki.drawLine(67, 23, 79, 23);
+  sparki.drawLine(79, 19, 79, 23);
+  //draw
+  sparki.updateLCD();
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  draw_face();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ final_project/LCD_Drawrings/draw_knife.ino
@@ -0,0 +1,24 @@
+void draw_knife(){
+  sparki.clearLCD();
+  //spine
+  sparki.drawLine(27, 47, 79, 7);
+  //point
+  sparki.drawLine(27, 47, 35, 47);
+  //blade
+  sparki.drawLine(35, 47, 81, 19);
+  //butt
+  sparki.drawLine(81, 19, 73, 12);
+  //hilt
+  sparki.drawLine(79, 7, 81, 9);
+  sparki.drawLine(81, 9, 75, 14);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
