c986b0f7a1568ec9e6127754a4a2b5d5e62216df
cschneck <cschneck.noreply.github.com>
2017-12-02 19:06:05 -0700

    find light function, switch to found when light is found

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -15,25 +15,32 @@ Douglas Allen
 #include <Sparki.h> // include sparki library
 
 // Define states for following light (sensor states)
-#define HARD_RIGHT 1 //001
-#define SOFT_RIGHT 11 //011
-#define HARD_LEFT 100
-#define FORWARD_CENTER 10 //010
-#define SOFT_LEFT 110
+// 1 (SEES LIGHT), 0 (DOES NOT SEE LIGHT)
 #define ALL_LIGHT 111
 #define NO_LIGHT 0 ///000
+#define  ONLY_RIGHT 1 //001
+#define  RIGHT_CENTER 11 //011
+#define  ONLY_LEFT 100
+#define  LEFT_CENTER 110
+#define  ONLY_CENTER 10 //010
 
 // Define states for what action the robot is taking (program states)
 #define FIND_LIGHT 0 // attempting to find victims
-#define FOUND_VICTIM 1 // sees light, follows
-#define RETURN_HOME 2 // return victim to home, drop victim, return to goal node
-#define END_GAME 3 // all victims have been captured, or lost, end of all movement
+#define FOUND_LIGHT 1 // found light, implements best path algorithm
+#define FOLLOW_LIGHT 2 // following best path to light source
+#define LOCAL_SEARCH 3 // gets to approx. locationa and searches for victim with ping
+#define FOUND_VICTIM 4 // found objects, appraoching to grab
+#define RETURN_HOME 5 // grabbed victim, returning home
+#define END_GAME 6 // all victims have been captured, or lost, end of all movement
 
 const int threshold = 500; // light sensor threshold
 bool lightLeft, lightCenter, lightRight = false; // light senesors see light above threshold
 
 int light_state = NO_LIGHT;
+bool found_light = false;
 int program_state = FIND_LIGHT;
+
+int ping = 0;
 
 String state = "undefined"; // print program state on display
 
@@ -43,6 +50,9 @@ void setup() {
   delay(300);
 }
 
+/*******************************************
+TAKES READINGS FROM SENSORS TO DISPLAY
+********************************************/
 void displaySensorsAndStates()
 {
   sparki.clearLCD(); // wipe screen clean each run  
@@ -50,24 +60,79 @@ void displaySensorsAndStates()
   sparki.println(light_state);
   
   sparki.println(String("state = ") + program_state); // displays state of movement (follow line, approaching, etc...)
+  sparki.println("state = " + state); // displays state of movement (follow line, approaching, etc...)
   
   sparki.updateLCD(); //display all information written to screen
 }
 
 void readSensors()
 {
-  // sensor is 1 if reading white and 0 if reading black
-  lightLeft = sparki.lightLeft() > threshold; // considered to be on the path if greater than threshold
+  // sensor is 1 if sees light and 0 if does not see light 
+  lightLeft = sparki.lightLeft() > threshold;
   lightCenter = sparki.lightCenter() > threshold;
   lightRight = sparki.lightRight() > threshold;
 
-  // determine move state
-  // left = 100 -> hard left (only left) = 100, soft left = left + center = 100 + 010 = 110
-  // center = 010
-  // right = 001 -> hard right (only right) = 001, or  soft right = right + center = 001 + 010 = 011
-  // all light = 111, no light = 000
-  // false is 0, true is 1
-  light_state = (!lightLeft*100)+(!lightCenter*10)+(!lightRight*1); // creates unique 000 number combination to represent each possible sensor combintation (e.g. BLACKBLACKWHITE, etc...)
+  light_state = (lightLeft*100)+(lightCenter*10)+(lightRight*1); // creates unique 000 number combination to represent each possible sensor combintation (e.g. BLACKBLACKWHITE, etc...)
+
+  if (light_state) 
+  {
+    program_state = FOUND_LIGHT;
+    state = "light found";
+  }
+}
+
+void findLight() {
+  state = "find light";
+  if (program_state == FIND_LIGHT) // while program hasn't found light
+  {
+    readSensors();
+    displaySensorsAndStates();
+  }
+}
+
+void lightStates()
+{
+  readSensors(); // continually grab new sensor data for left/center/right sensors
+  displaySensorsAndStates(); // display sensor on display
+
+  switch(light_state) 
+  {
+    //case HARD_LEFT: moveLeft(); break;
+    case ONLY_RIGHT: break;
+    case RIGHT_CENTER: break;
+    case ONLY_LEFT: break;
+    case LEFT_CENTER: break;
+    case ONLY_CENTER: break;
+    case ALL_LIGHT: break;
+    case NO_LIGHT: break; // keeps doing previous action
+    default: break; // keeps doing previous action
+  }
+}
+
+void programStates()
+{
+/*
+// Define states for what action the robot is taking (program states)
+#define FIND_LIGHT 0 // attempting to find victims
+#define FOUND_LIGHT 1 // found light, implements best path algorithm
+#define FOLLOW_LIGHT 2 // following best path to light source
+#define LOCAL_SEARCH 3 // gets to approx. locationa and searches for victim with ping
+#define FOUND_VICTIM 4 // found objects, appraoching to grab
+#define RETURN_HOME 5 // grabbed victim, returning home
+#define END_GAME 
+*/
+
+  switch(program_state)
+  {
+    case FIND_LIGHT: findLight(); break;
+    case FOUND_LIGHT: break;
+    // case FOLLOW_LIGHT: break;
+    // case LOCAL_SEARCH: break;
+    // case FOUND_VICTIM: break;
+    // case RETURN_HOME: break;
+    // case END_GAME: break;
+    default: break;
+  }
 }
 
 void loop() {
@@ -75,6 +140,7 @@ void loop() {
   if (program_state != END_GAME) // while the program isn't done
   {
     readSensors();
+    programStates();
     displaySensorsAndStates();
     delay(10); // loops .01 a second (pings rate)
   }
