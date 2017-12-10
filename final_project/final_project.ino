7ab7ee5d66f4b7d49c6abf6819419c0f5e58e7d7
Anthony Hauger <anthonyhauger@Anthonys-MacBook-Air.local>
2017-12-09 21:39:44 -0700

    It at least uploads now.

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -14,10 +14,6 @@ Douglas Allen
 ********************************************/
 #include <Sparki.h> // include sparki library
 
-#define infinity 999
-
-/* BEGIN STATE DEFINITION FOR LIGHT FOLLOWING */
-
 // Define states for following light (sensor states)
 // 1 (SEES LIGHT), 0 (DOES NOT SEE LIGHT)
 #define ALL_LIGHT 111
@@ -27,9 +23,7 @@ Douglas Allen
 #define  ONLY_LEFT 100
 #define  LEFT_CENTER 110
 #define  ONLY_CENTER 10 //010
-/* END LIGHT STATE DEFINITION */
 
-/* BEGIN ROBOT ACTION DEFINITION */
 // Define states for what action the robot is taking (program states)
 #define FIND_LIGHT 0 // attempting to find victims
 #define FOUND_LIGHT 1 // found light, implements best path algorithm
@@ -38,124 +32,39 @@ Douglas Allen
 #define FOUND_VICTIM 4 // found objects, appraoching to grab
 #define RETURN_HOME 5 // grabbed victim, returning home
 #define END_GAME 6 // all victims have been captured, or lost, end of all movement
-/* END ROBOT ACTION DEFINITION */
 
-/* BOARD SIZE FOR ODOMETRY */
-#define  GRID_WIDTH  15.25 //GRID_WIDTHxGRID_HEIGHT is the size of the Sparki box
-#define GRID_HEIGHT 11.75
+#define infinity 999
+/*Dijkstras Variables*/
+int start_node=1;
+int current_node=1;
+int goal_node=0;
 
-#define HALF_WIDTH 7.625 //midpoint of each grid is H_W*jxH_H*i
-#define HALF_HEIGHT 5.875
-/* END BOARD SIZE */
+int numNodes=16;
+int distanceToNode[16];
+int go_to[16];
 
-/* INITILIZE LIGHT VARIABLES */
-const int left_threshold = 70; // left light sensor threshold
-const int right_threshold = 70; //right light sensor threshold
-const int center_threshold = 20; //center light sensor threshold
+int grid[4][4]={
+  {0,0,0,0},
+  {0,1,1,0},
+  {0,1,0,0},
+  {0,0,0,1}
+};
+/*End Dijkstra Variables*/
+
+const int threshold = 500; // light sensor threshold
 bool lightLeft, lightCenter, lightRight = false; // light senesors see light above threshold
-int left=sparki.lightLeft();
-int center=sparki.lightCenter();
-int right=sparki.lightRight();
+
 int light_state = NO_LIGHT;
 bool found_light = false;
 int program_state = FIND_LIGHT;
-/* END INITIALIZE LIGHT VARS */
 
 int ping = 0;
 
-
-/* START DIJKSTRA SETUP */
-int numRows=5;
-int numColumns=5;
-int numNodes=25;
-int distanceToNode[25];
-int go_to[25];
-
-bool map_space[5][5]={ //1's are obstacles!
-  {0, 0, 0, 0, 0},
-  {0, 0, 1, 0, 0},
-  {0, 1, 1, 0, 1},
-  {0, 0, 0, 0, 0},
-  {0, 0, 1, 0, 0}
-};
-
-int startPosition=1;
-int currentPosition=1;
-int goalPosition=24;
-/* END DIJKSTRA SETUP */
-
 String state = "undefined"; // print program state on display
 
-/* DIJSTRAS RELATED FUNCTIONS */
-
-int cost(int start, int goal){
-  if (start == goal){
-    return 0;
-  }
-  
-  int dist=0;
-  int x1=start/numRows;
-  int x2=goal/numRows;
-  int y1=start%numColumns;
-  int y2=goal%numColumns;
-
-  if(map_space[x1][y1]==1 || map_space[x2][y2]==1){
-    return infinity;
-  }
-  
-  dist=abs(x2-x1)+abs(y2-y1);
-
-  if(dist>1){
-    return infinity;
-  } else {
-    return 1;
-  }
-}
-
-void dijkstra(int n, int v, int distance[]){ //n=number of nodes, v=goal node
-  int i,u,count,w,flag[numColumns*numRows],minimum;
-  for(i=0; i<n; i++){
-    flag[i]=0;
-    distance[i]=cost(v, i);
-  }
-  distance[v]=0; //copied from Correll's file, maybe gives better sense for sparki to want to go there?
-  count=1;
-  while(count<=n){
-    minimum=infinity;
-    for(w=0;w<n;w++){
-      if(distance[w]<minimum && !flag[w]){
-        minimum=distance[w];
-        u=w;
-      }
-      flag[u]=1;
-      count++;
-      for(w=0;w<n;w++){
-        if((distance[u]+cost(u,w)<distance[w]) && !flag[w]){
-          distance[w]=distance[u]+cost(u,w);
-          go_to[w]=u;
-        }
-      }
-    }
-  }
-
-  /* PRINT TO MAKE SURE IT DOES THE GOAL NODE PATH CORRECTLY */
-  sparki.clearLCD();
-  sparki.println("path to goal:");
-  for(w=0;w<n;w++){
-    sparki.print(go_to[w]);
-  }
-  sparki.updateLCD();
-}
-
-byte positionToNode(byte positionRow, byte positionColumn){
-  return((positionRow*numRows)+positionColumn);
-}
-
-/* END DIJKSTRAS RELATED FUNCTIONS */
-
 void setup() {
-  for (int node=0; node<(numRows*numColumns); node++){
-    distanceToNode[node]=infinity;
+  for(int i=0;i<numNodes;i++){
+    distanceToNode[i]=infinity;
   }
   sparki.servo (SERVO_CENTER);
   sparki.clearLCD();
@@ -168,59 +77,35 @@ TAKES READINGS FROM SENSORS TO DISPLAY
 void displaySensorsAndStates()
 {
   sparki.clearLCD(); // wipe screen clean each run  
-
-  sparki.print("Left=");
-  sparki.println(left);
-  sparki.print("Center=");
-  sparki.println(center);
-  sparki.print("\nRight=");
-  sparki.println(right);
   if(program_state==FIND_LIGHT){
     sparki.print("Light state: ");
     sparki.println(light_state);
   } else if(program_state==FOUND_LIGHT){
-    sparki.print("Goal node:\n");
-    sparki.println(goalPosition);
+    sparki.print("Goal node:");
+    sparki.println(goal_node);
+  } else if(program_state==FOLLOW_LIGHT){
+    for(int i=0;i<16;i++){
+         if(distanceToNode[i]!=infinity) 
+          Serial.print(go_to[i]);
+         else
+          Serial.print("#");
+        Serial.print(" ");
+        if((i+1)%4==0) Serial.println();
+    }
+  Serial.println();
   }
-  sparki.println(String("\nstate = ") + program_state); // displays state of movement (follow line, approaching, etc...)
-  sparki.println("\nstate = " + state); // displays state of movement (follow line, approaching, etc...)
+  sparki.println(String("state = ") + program_state); // displays state of movement (follow line, approaching, etc...)
+  sparki.println("state = " + state); // displays state of movement (follow line, approaching, etc...)
   
   sparki.updateLCD(); //display all information written to screen
 }
 
-/* SECOND STATE FUNCTION */
-void setGoal(){ //branches are not in numerical order, but in direction order
-  if(light_state==1){ //directly to Sparki's left
-    goalPosition=2;
-  } else if(light_state==11){ //front left of Sparki
-    goalPosition=13;
-  } else if(light_state==10){ //directly in front of Sparki
-    goalPosition=15;
-  } else if(light_state==110){ //front right of Sparki
-    goalPosition=5;
-  } else if(light_state==100){ //directly right of Sparki
-    sparki.moveRight(100);
-    program_state=LOCAL_SEARCH;
-    return;
-  } else { //either 000 or 111
-    program_state=FIND_LIGHT;
-    return;
-  }
-
-  dijkstra(numNodes, goalPosition, distanceToNode);
-  displaySensorsAndStates();
-
-  program_state=FOLLOW_LIGHT;
-}
-/* END SECOND STATE FUNCTION */
-
-/* BEGIN FIRST STATE FUNCITONS */
 void readSensors()
 {
   // sensor is 1 if sees light and 0 if does not see light 
-  lightLeft = sparki.lightLeft() > left_threshold;
-  lightCenter = sparki.lightCenter() > center_threshold;
-  lightRight = sparki.lightRight() > right_threshold;
+  lightLeft = sparki.lightLeft() > threshold;
+  lightCenter = sparki.lightCenter() > threshold;
+  lightRight = sparki.lightRight() > threshold;
 
   light_state = (lightLeft*100)+(lightCenter*10)+(lightRight*1); // creates unique 000 number combination to represent each possible sensor combintation (e.g. BLACKBLACKWHITE, etc...)
 
@@ -272,9 +157,83 @@ void lightStates()
     default: break; // keeps doing previous action
   }
 }
-/* END FIRST STATE FUNCTIONS */
 
-/* BEGIN LOOP FUNCTIONS */
+void setGoal(){
+  if(light_state==1){
+    goal_node=2;
+    displaySensorsAndStates();
+    program_state=FOLLOW_LIGHT;
+  } else if(light_state==11){
+    goal_node=11;
+    displaySensorsAndStates();
+    program_state=FOLLOW_LIGHT;
+  } else if(light_state==10){
+    goal_node=12;
+    displaySensorsAndStates();
+    program_state=FOLLOW_LIGHT;
+  } else if(light_state==110){
+    goal_node=4;
+    displaySensorsAndStates();
+    program_state=FOLLOW_LIGHT;
+  } else if(light_state==100){
+    sparki.moveRight(100);
+    program_state=LOCAL_SEARCH;
+  } else {
+    program_state=FIND_LIGHT;
+  }
+}
+
+
+int cost(int i, int j){
+  int x1= i/4;
+  int y1= i%4;
+  int x2= j/4;
+  int y2= j%4;
+
+  if(i==j){
+    return 0;
+  }
+  if(grid[x1][y1] || grid[x2][y2]){ //if obstacle
+    return(infinity);
+  }
+  int cost = fabs(x1-x2)+fabs(y1-y2);
+  if(cost>1){
+    return(infinity);
+  } else {
+    return(cost);
+  }
+}
+
+void dij(int n, int v, int dist[]){
+  int i,u,count,w,flag[16],minimum;
+
+  for(i=0;i<=n;i++){
+    flag[i]=0;
+    dist[i]=99;
+  }
+  dist[start_node]=0;
+  count=1;
+  while(count<=n){
+    minimum=infinity;
+    for(w=0;w<=n;w++){
+      minimum=dist[w];
+      u=w;
+    }
+    flag[u]=1;
+    count++;
+    for(w=0;w<=n;w++){
+      if((dist[u]+cost(u,w)<dist[w])&& !flag[w]){
+        dist[w]=dist[u]+cost(u,w);
+        go_to[w]=u;
+      }
+    }
+  }
+}
+
+void call_dijkstras(){
+  dij(numNodes, goal_node, distanceToNode);
+  displaySensorsAndStates();
+}
 void programStates()
 {
 /*
@@ -291,9 +250,9 @@ void programStates()
   switch(program_state)
   {
     case FIND_LIGHT: findLight(); break;
-    case FOUND_LIGHT: setGoal; break;
-    case FOLLOW_LIGHT: break;
-    // case LOCAL_SEARCH: break;
+    case FOUND_LIGHT: setGoal(); break;
+    case FOLLOW_LIGHT: call_dijkstras(); break;
+    case LOCAL_SEARCH: break;
     // case FOUND_VICTIM: break;
     // case RETURN_HOME: break;
     // case END_GAME: break;
@@ -311,5 +270,3 @@ void loop() {
     delay(10); // loops .01 a second (pings rate)
   }
 }
-
-/* END LOOP FUNCTIONS */
